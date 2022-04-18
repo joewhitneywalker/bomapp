2d13e8be1a9c1575a69a87b5d32dbffc9995cedd
joewhitneywalker <walker.joe81@gmail.com>
2022-04-17 17:35:05 -0700

    adding static styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
 
 
 
 # Create your views here.
 
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("HOME")
+class Home(TemplateView):
+    template_name = 'home.html'
