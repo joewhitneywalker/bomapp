ee16cea84c32a4a1a4034b1f5ce010ea978034eb
joewhitneywalker <walker.joe81@gmail.com>
2022-04-20 19:45:45 -0700

    added component model and tested

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bom
+from .models import Bom, Component
 
 # Register your models here.
 admin.site.register(Bom)
+admin.site.register(Component)
