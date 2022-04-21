05c4bc2f6fb20324b5aa2413c4a422152b2e55a2
joewhitneywalker <walker.joe81@gmail.com>
2022-04-20 20:21:36 -0700

    updated and connected models with help from Nicole

--- main_app/models.py
+++ main_app/models.py
@@ -2,23 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Bom(models.Model):
-    
-    style_name= models.CharField(max_length=200)
-    style_number= models.CharField(max_length=5)
-    created_by = models.CharField(max_length=25)
-    created_at = models.DateTimeField(auto_now_add=True)
-    season = models.CharField(max_length=4, default='FA20', editable=True)#MAIN SQL QUERY
-    category = models.CharField(max_length=25, default='OUTERWEAR', editable=True)
-    colorway = models.CharField(max_length=25)
-    img = models.CharField(max_length=250)
-
-def __str__(self):
-        return self.style_name
-class Meta:
-        ordering = ['style_number']
-
-
 class Component(models.Model):
 
     name = models.CharField(max_length=50)
@@ -34,3 +17,24 @@ class Component(models.Model):
 
 def __str__(self):
         return self.name
+
+
+
+class Bom(models.Model):
+    
+    style_name= models.CharField(max_length=200)
+    style_number= models.CharField(max_length=5)
+    created_by = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add=True)
+    season = models.CharField(max_length=4, default='FA20', editable=True)#MAIN SQL QUERY
+    category = models.CharField(max_length=25, default='OUTERWEAR', editable=True)
+    colorway = models.CharField(max_length=25)
+    img = models.CharField(max_length=250)
+    component = models.ManyToManyField(Component)
+
+def __str__(self):
+        return self.style_name
+class Meta:
+        ordering = ['style_number']
+
+
